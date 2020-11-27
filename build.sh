8d369ea55c1bc091fd6053463817a0d47fc3cd3d
Henrique Couto <email@henriquecouto.com.br>
2020-11-26 23:15:41 -0300

    remove make file and move build responsability to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 rm miniature_client
 rm miniature_server
-rm *.o
 
-make -f Makefile.miniature
+# compile client
+gcc miniature_clnt.c miniature_client.c miniature_xdr.c -o miniature_client -lm
+
+#compile server
+gcc miniature_server.c miniature_svc.c miniature_xdr.c server/*.c -o miniature_server -lm
